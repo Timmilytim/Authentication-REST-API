f293c548654819c37982e6d982324ee992a15747
Oluwatimilehin Odusanya <oluwapelumiodus@gmail.com>
2025-08-11 23:58:34 +0100

    Protecting the route in my SecurityConfig file

--- src/main/java/com/auth/config/SecurityConfig.java
+++ src/main/java/com/auth/config/SecurityConfig.java
@@ -26,6 +26,7 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/user/profile").authenticated()
                         .requestMatchers(
                                 "/api/auth/signup",
                                 "/api/auth/login",
